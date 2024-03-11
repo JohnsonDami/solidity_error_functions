bcf840adf4a44d07d14769e765a27dee49dcaff3
JohnsonDami Damilola <johnsondamilola2004@gmail.com>
2024-03-11 12:04:21 +0000

    Added Owner Contract

---
+++ contracts/Owner.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Owned {
+    address public owner;
+
+    constructor (address _owner) {
+        require(_owner !== address(0), "Cannot set zero address as owner");
+        owner = _owner;
+    }
+
+    function changeOwner(address _newOwner) external {
+        if (msg.sender != owner) {
+            revert("Only Owner can run this function");
+        }
+        assert(_newOwner != address(0));
+        owner = _newOwner;
+    }   
+}
